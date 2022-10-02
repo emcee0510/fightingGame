f22f011223815f683ecf84f7267e7c7b176b6a8c
emcee0510 <113955507+emcee0510@users.noreply.github.com>
2022-10-03 02:13:12 +0800

    scene 2

--- FightingGame/Assets/gameManager.cs
+++ FightingGame/Assets/gameManager.cs
@@ -1,11 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class gameManager : MonoBehaviour
 {
-    public float time;
-    public GameObject GoTime;
+   
+    public static int P1Health;
+    public static int P2Health;
+    public static String PlayerOne;
+    public static String PlayerTwo;
+    public GameObject p1HP;
+    public GameObject p2HP;
     public GameObject Player1;
     public GameObject Player2;
     public GameObject InputOne;
@@ -13,24 +21,41 @@ public class gameManager : MonoBehaviour
 
     // Start is called before the first frame update
     void Start(){
-
+        
     }
 
     // Update is called once per frame
     void Update(){
 
-       GoTime.gameObject.GetComponent<TMPro.TextMeshProUGUI>().text = time.ToString("0") + "";
+        p1HP.gameObject.GetComponent<TMPro.TextMeshProUGUI>().text = P1Health + "";
+        p2HP.gameObject.GetComponent<TMPro.TextMeshProUGUI>().text = P2Health + "";
+        Player1.gameObject.GetComponent<TMPro.TextMeshProUGUI>().text = PlayerOne + "";
+        Player2.gameObject.GetComponent<TMPro.TextMeshProUGUI>().text = PlayerTwo + "";
     }
 
     void FixedUpdate(){
-        
-        time -= Time.deltaTime;
+
+       
     }
 
-    public void InputNames()
+    public void Inputs()
     {
-        Player1.gameObject.GetComponent<TMPro.TextMeshProUGUI>().text = InputOne.gameObject.GetComponent<TMPro.TextMeshProUGUI>().text + "";
-        Player2.gameObject.GetComponent<TMPro.TextMeshProUGUI>().text = InputTwo.gameObject.GetComponent<TMPro.TextMeshProUGUI>().text + "";
+       PlayerOne = InputOne.gameObject.GetComponent<TMPro.TextMeshProUGUI>().text;
+       PlayerTwo = InputTwo.gameObject.GetComponent<TMPro.TextMeshProUGUI>().text;
+        SceneManager.LoadScene(1);
 
     }
+
+    public void SetHealth50()
+    {
+        P1Health = 50;
+        P2Health = 50;
+    }
+
+    public void SetHealth100()
+    {
+        P1Health = 100;
+        P2Health = 100;     
+    }
+
 }
